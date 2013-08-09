546b9144c103b9c58f0c29dac4df73679911063d
jansesun <307665751@qq.com>
2013-08-09 18:12:47 +0800

    advanced understand about setTimeout

---
+++ advanced-for-setTimeout.js
@@ -0,0 +1,52 @@
+var func1 = function(times) {
+  var innerFun = function() {
+		setTimeout(function(){
+			if(times) {
+				console.log('func1');
+				innerFun();
+				times--;
+			}
+		}, 50);
+	}
+	innerFun();
+};
+var func2 = function() {
+	setTimeout(function() {
+		console.log('func2');
+	}, 100);
+};
+//print 'func2' after print 'func1'
+//bad example
+func1(3);
+setTimeout(function() {
+	func2();
+}, 50);
+//
+var func1 = function(times, call) {
+	var innerFun = function() {
+		setTimeout(function(){
+			if(times) {
+				//codes go here
+				innerFun();
+				times--;
+			} else {
+				if(call) {
+					call();
+				}
+			}
+		}, 50);
+	}
+	innerFun();
+};
+var func2 = function() {
+	setTimeout(function() {
+		console.log('func2');
+	}, 100);
+};
+//print 'func2' after print 'func1'
+//bad example
+func1(3, function(){
+	func2();
+});
+// by this way, we can make sure func2 will be executed after the 3 times innerFun
+
